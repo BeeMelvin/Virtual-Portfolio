109344af153b45e03544cb37fe38f50930758dee
BeeMelvin <hamalepeb@gmail.com>
2024-07-31 20:01:35 +0200

    font settings

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ const sora = Sora({
   variable: '--font-sora',
   weight: ['100','200','300','400','500','600','700','800'],
 });
-//
+// componnets
 const Layout = () => {
   return <div>Layout</div>;
 };
