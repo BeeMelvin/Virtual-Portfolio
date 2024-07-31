a5c73ce22b1d7910e3ce490efd92a499da1b0c18
BeeMelvin <hamalepeb@gmail.com>
2024-07-31 23:29:20 +0200

    aligning icons and making them to float

--- components/Nav.js
+++ components/Nav.js
@@ -33,15 +33,33 @@ import Link from 'next/link'
 //next router
 import { useRouter } from 'next/router';
 
-
+import classNames from 'classNames';
 const Nav = () => {
+  const router =useRouter();
+  const pathname = router.pathname;
   return (
-  <nav>
-    {/*inner */}
-    <div>
+  <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max
+  bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+    
+    <div className='flex w-full xl:flex-col items-center justify-between
+    xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h[-80] xl:h-max py-8 bg-white/10
+    backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
       {navData.map((link, index) => {
-        return <Link href={link.path}>{link.icon}
-        </Link>;
+        return (
+
+        
+        <Link 
+         className={classNames({
+        'text-accent': link.path === pathname,
+          'relative flex items-center group hover:text-accent transition-all duration=300': true,
+        })}
+        href ={link.path}
+        key={index}
+        >
+
+          <div>{link.icon}</div>
+        </Link>
+        );
       })}
     </div>
   </nav>
