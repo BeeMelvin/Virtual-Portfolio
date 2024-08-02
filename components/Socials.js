cb459774847c538626895ef70827c61a11621827
BeeMelvin <hamalepeb@gmail.com>
2024-08-02 17:40:55 +0200

    inserting links for socials

--- components/Socials.js
+++ components/Socials.js
@@ -14,19 +14,19 @@ import {
 const Socials = () => {
   return (
     <div className='fixed top-4 right-4 flex flex-col gap-5 md:flex-row md:gap-x-5 md:top-8 md:right-8'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://youtube.com/@beekay_sa?si=CSgyYaAeZckvFf6n'} className='hover:text-accent transition-all duration-300'>
         <RiYoutubeLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.facebook.com/boikanyo.malepe.7?mibextid=ZbWKwL'} className='hover:text-accent transition-all duration-300'>
         <RiFacebookLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.instagram.com/beekay_mv?igsh=bmp1OTR5bDE4dXhr'} className='hover:text-accent transition-all duration-300'>
         <RiInstagramLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/boikanyo-malepe-b70931219?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'} className='hover:text-accent transition-all duration-300'>
         <RiLinkedinLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://github.com/BeeMelvin'} className='hover:text-accent transition-all duration-300'>
         <RiGithubLine/>
       </Link>
      
