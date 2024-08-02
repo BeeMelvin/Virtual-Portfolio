62461747d91f1318487231810969312e4aa6a3ce
BeeMelvin <hamalepeb@gmail.com>
2024-08-02 06:57:27 +0200

    Work, exerience, skills, changing backgrounds

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const Avatar = () => {
   return <div className='hidden xl:flex xl:max-w-none'>
-    <Image src={'/Avatar2.png'}
+   { /* <Image src={'/Avatar2.png'} 
      width={737}
       height={678}
        alt=''
-    className='translate-z-0-w-full h-full'/>
+    className='translate-z-0-w-full h-full'/> */}
   </div>;
 };
 
